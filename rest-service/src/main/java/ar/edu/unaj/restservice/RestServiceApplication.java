c9f1b5b3500863457e1de5476f58cdd79ce47f76
Walter Leguiza <wal.leguiza@gmail.com>
2018-12-12 10:30:06 -0300

    Added rest-service module

---
+++ rest-service/src/main/java/ar/edu/unaj/restservice/RestServiceApplication.java
@@ -0,0 +1,14 @@
+package ar.edu.unaj.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
+
